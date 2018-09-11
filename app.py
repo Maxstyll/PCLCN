dd2b3f6b279a6583fe68373aefad2e90e8f49cb5
Emerson Antonio da Silva <esilva@apoioprodesp.sp.gov.br>
2018-09-11 14:01:32 -0300

    dados para analise

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # Abrir arquivo
 # Convert arquivo para json
 # Fazer splite de frase para palavas
+# Garantir não ter palavras duplicadas
 # Insert dados em uma tabela SQLite
 # Identificar em qual classes variáveis a palavra se enquadra (artigo, adjetivo, pronome, numeral, substantivo e verbo)
 # Identificar em qual intenção ele se enquadra (fugir, subtrair, coação, simulando, portar, agreção, abordagem, quantidade, vestir, deslocar, veiculo, arma, Autor, caracteristicaAutor, acaoVitiva, bensVitima, outros)
