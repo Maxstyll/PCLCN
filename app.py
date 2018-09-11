7542eb36253f953b8aa3ff9f58153997a466bd69
Emerson Antonio <afirmanet@gmail.com>
2018-09-11 11:21:43 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+# Inicial aplicativo
+# Abrir arquivo
+# Convert arquivo para json
+# Fazer splite de frase para palavas
+# Insert dados em uma tabela SQLite
+# Identificar em qual classes variáveis a palavra se enquadra (artigo, adjetivo, pronome, numeral, substantivo e verbo)
+# Identificar em qual intenção ele se enquadra (fugir, subtrair, coação, simulando, portar, agreção, abordagem, quantidade, vestir, deslocar, veiculo, arma, Autor, caracteristicaAutor, acaoVitiva, bensVitima, outros)
