1219094af05c05ace35ea64bf75067efe6638283
Emerson Antonio da Silva <esilva@apoioprodesp.sp.gov.br>
2018-09-12 12:37:17 -0300

    Atualização

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
+# Import dos pacotes
+import os
+import glob
+import json
+import codecs
+
 # Inicial aplicativo
+
 # Abrir arquivo
-# Convert arquivo para json
-# Fazer splite de frase para palavas
-# Garantir não ter palavras duplicadas
+path = './doc'
+for filename in glob.glob(os.path.join(path, '*.json')):
+    with open(filename, 'r') as keys_file:
+        #keys = keys_file.read().encode('utf-8')
+        #keys_json = json.loads(keys)
+
+        keys_json = json.load(keys_file) # Convert arquivo para json
+        acaoAutor = keys_json["acaoAutor"]
+        for text in acaoAutor:
+            vtTexto = text.splite # Fazer splite de frase para palavas
+            if(vtTexto.length > 1):
+                print(vtTexto) #Gravar dados para regra de contesto semantico
+
+            for palavra in vtTexto:
+                print(palavra)
+
+# Garantir nao ter palavras duplicadas
 # Insert dados em uma tabela SQLite
-# Identificar em qual classes variáveis a palavra se enquadra (artigo, adjetivo, pronome, numeral, substantivo e verbo)
-# Identificar em qual intenção ele se enquadra (fugir, subtrair, coação, simulando, portar, agreção, abordagem, quantidade, vestir, deslocar, veiculo, arma, Autor, caracteristicaAutor, acaoVitiva, bensVitima, outros)
+# Identificar em qual classes variaveis a palavra se enquadra (artigo, adjetivo, pronome, numeral, substantivo e verbo)
+# Identificar em qual intencao ele se enquadra (fugir, subtrair, coacao, simulando, portar, agrecao, abordagem, quantidade, vestir, deslocar, veiculo, arma, Autor, caracteristicaAutor, acaoVitiva, bensVitima, outros)
