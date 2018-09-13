30ebe5aae6cbd1b7f0dc6e02fb958bc8bbe32551
Emerson Antonio <afirmanet@gmail.com>
2018-09-13 05:54:23 -0300

    Separado as palavras e feito a gravacao no SQLite

--- app.py
+++ app.py
@@ -2,27 +2,59 @@
 import os
 import glob
 import json
-import codecs
+import sqlite3
+import polyglot
+
+from polyglot.text import Text, Word
 
 # Inicial aplicativo
+
+# Criar o Banco de Dados
+os.remove("./db/dadosDipol.db") if os.path.exists("./db/dadosDipol.db") else None
+con = sqlite3.connect('./db/dadosDipol.db')
+cur = con.cursor()
+
+# Criar as Tabelas miniFrase
+sql_create_miniFrase = 'create table miniFrases '\
+'(id integer primary key AUTOINCREMENT, '\
+'texto varchar(200), '\
+'arquivo varchar(140))'
+cur.execute(sql_create_miniFrase)
+sql_insert_miniFrase = 'insert into miniFrases (texto, arquivo) values (?, ?)'
+
+
+# Criar as Tabelas palavras
+sql_create_palavras = 'create table palavras '\
+'(id integer primary key AUTOINCREMENT, '\
+'palavra varchar(50), '\
+'tag varchar(10), '\
+'arquivo varchar(140))'
+cur.execute(sql_create_palavras)
+sql_insert_palavra = 'insert into palavras (palavra, tag, arquivo) values (?, ?, ?)'
+
 
 # Abrir arquivo
 path = './doc'
 for filename in glob.glob(os.path.join(path, '*.json')):
-    with open(filename, 'r') as keys_file:
-        #keys = keys_file.read().encode('utf-8')
-        #keys_json = json.loads(keys)
+    keys_file  = open(filename, 'r', encoding='utf8')
+    keys_json = json.loads(keys_file.read()) # Convert arquivo para json
+    acaoAutor = keys_json["acaoAutor"]
+    for text in acaoAutor:
+        vtTexto = text# Fazer splite de frase para palavas
+        text = Text(vtTexto)
+        for word, tag in text.pos_tags:
+            rec = (word, tag, filename)
+            cur.execute(sql_insert_palavra, rec)
 
-        keys_json = json.load(keys_file) # Convert arquivo para json
-        acaoAutor = keys_json["acaoAutor"]
-        for text in acaoAutor:
-            vtTexto = text.splite # Fazer splite de frase para palavas
-            if(vtTexto.length > 1):
-                print(vtTexto) #Gravar dados para regra de contesto semantico
+        vtTexto = vtTexto.split()
+        if(len(vtTexto) > 1):
+            print(text) #Gravar dados para regra de contesto semantico
+            rec = (text, filename)
+            cur.execute(sql_insert_miniFrase, rec)
 
-            for palavra in vtTexto:
-                print(palavra)
+        con.commit()
 
+    
 # Garantir nao ter palavras duplicadas
 # Insert dados em uma tabela SQLite
 # Identificar em qual classes variaveis a palavra se enquadra (artigo, adjetivo, pronome, numeral, substantivo e verbo)
